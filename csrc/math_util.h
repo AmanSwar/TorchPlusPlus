2fb739dcc10e29eae1eeeddbe7c48e78a844dd80
Aman <aman.swar001@gmail.com>
2025-10-28 08:06:11 +0530

    math util init

---
+++ csrc/math_util.h
@@ -0,0 +1,9 @@
+
+namespace torchpp{
+
+#define T_SQRT_2 1.41421356237309504880 // sqrt(2)
+
+#define T_I_SQRT_2 0.70710678118654752440 // 1 / sqrt(2)
+#define T_2_I_SQRT_PI 1.12837916709551257390 // 2 / sqrt(pi)
+
+}
