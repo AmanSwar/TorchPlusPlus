f15f88506dbd359f0a0ac9ba937837f21ec4b279
Aman <aman.swar001@gmail.com>
2025-11-02 22:54:45 +0530

    feat : ceil division function

--- csrc/math_util.h
+++ csrc/math_util.h
@@ -6,4 +6,11 @@ namespace torchpp{
 #define T_I_SQRT_2 0.70710678118654752440 // 1 / sqrt(2)
 #define T_2_I_SQRT_PI 1.12837916709551257390 // 2 / sqrt(pi)
 
+
+// math utils
+__device__ __host__ __forceinline__ int divCeil(int a, int b) {
+  return (a % b != 0) ? (a / b + 1) : (a / b);
+}
+
+
 }
