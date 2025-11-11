139874bb9534e83b64d460b2b7aec42659bc9b01
Aman <aman.swar001@gmail.com>
2025-11-11 18:08:49 +0530

    feat[activation] : add LinearGELU and LinearSILU

---
+++ torchpp/dlops/fused_linear.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+import linearActvationFp16
+
+
+class LinearGELU(nn.Module):
+
+  def __init__(self , in_features , out_features):
+    """
+    Linear layer with fused GELU activation
+
+    Args:
+        in_features : int = input dim
+        out_features : int = output dim
+    """
+    super().__init__()
+
+    self.weight = nn.Parameter(
+      torch.randn(out_features , in_features , dtype=torch.float16)
+    )
+
+  def forward(self , x):
+    return linearActvationFp16.LinearGelu(x , self.weight)
+  
+
+class LinearSILU(nn.Module):
+
+  def __init__(self , in_features , out_features):
+    """
+    Linear layer with fused SILU activation
+
+    Args:
+        in_features : int = input dim
+        out_features : int = output dim
+    """
+    super().__init__()
+
+    self.weight = nn.Parameter(
+      torch.randn(out_features , in_features , dtype=torch.float16)
+    )
+
+  def forward(self , x):
+    return linearActvationFp16.LinearSilu(x , self.weight)
+  
+  
