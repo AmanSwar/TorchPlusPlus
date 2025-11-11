e4eaf03f9ad32d2b88fbb5c865634ca46765a929
Aman <aman.swar001@gmail.com>
2025-11-11 19:15:34 +0530

    update[setup] : move loading from using load() to setup()

--- setup.py
+++ setup.py
@@ -1,21 +1,64 @@
-from torch.utils.cpp_extension import load
+from setuptools import setup
+from torch.utils.cpp_extension import load , BuildExtension , CUDAExtension
 import os
-CUTLASS_PATH = os.environ.get('CUTLASS_PATH', 'home/aman/cutlass/include')
-print(CUTLASS_PATH)
+
+
+CUTLASS_PATH = os.environ.get('CUTLASS_PATH', '~/cutlass/include')
+CUTLASS_PATH = os.path.abspath(os.path.expanduser(CUTLASS_PATH))
+
+
 CUDA_ARCH = os.environ.get('CUDA_ARCH', '80')
 
-cutlass_linear_gelu = load(
-    name="cutlass_linear_gelu_fp16",
-    sources=["csrc/activation/binding.cu"],
-    extra_cuda_cflags=[
-        "-O3",
-        "-std=c++17",
-        f"-I{CUTLASS_PATH}",
-        "--expt-relaxed-constexpr",
-        "--expt-extended-lambda",
-        f"-gencode=arch=compute_{CUDA_ARCH},code=sm_{CUDA_ARCH}",
-        "-Xptxas=-v",  # Verbose register usage
+gencode_flag = f"-gencode=arch=compute_{CUDA_ARCH},code=sm_{CUDA_ARCH}"
+
+setup(
+    name="LinearActivationFp16",
+    ext_modules=[
+        CUDAExtension(
+            name="linearActvationFp16",
+            sources=["csrc/activation/binding.cu"],
+            extra_compile_args={
+                "cxx": [
+                    "-std=c++17",
+                    "-O3",
+                    "-fPIC",
+                ],
+                "nvcc": [
+                    "-O2",
+                    # keep std for host compiler compatibility; nvcc accepts it too
+                    "-std=c++17",
+                    f"-I{CUTLASS_PATH}",
+                    "--expt-relaxed-constexpr",
+                    "--expt-extended-lambda",
+                    gencode_flag,
+                    "-Xptxas=-v",
+                    "-Xcompiler", "-fPIC",
+                ],
+            },
+        ),
+        CUDAExtension(
+            name="ropeApplyFunction",   
+            sources=["csrc/dlops/rope/rope_binding.cu"],
+            extra_compile_args={
+                "cxx": [
+                    "-std=c++17",
+                    "-O3",
+                    "-fPIC",
+                ],
+                "nvcc": [
+                    "-O2",
+                    # keep std for host compiler compatibility; nvcc accepts it too
+                    "-std=c++17",
+                    f"-I{CUTLASS_PATH}",
+                    "--expt-relaxed-constexpr",
+                    "--expt-extended-lambda",
+                    gencode_flag,
+                    "-Xptxas=-v",
+                    "-Xcompiler", "-fPIC",
+                ],
+            },
+            
+        )
     ],
-    extra_ldflags=[],
-    verbose=True
+    cmdclass={"build_ext": BuildExtension},
 )
