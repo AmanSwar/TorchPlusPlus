a6a3025fd0179a9822c0a8c32238c7e0b2a81d68
Aman <aman.swar001@gmail.com>
2025-11-16 20:18:41 +0530

    fix[kvcache] : fix pre-allocation of KV-cache resulting in OOM

--- torchpp/llmops/kvcache.py
+++ torchpp/llmops/kvcache.py
@@ -24,25 +24,34 @@ class KVCache:
     self.dtype = dtype
     self.device = device
 
-
-    self.k_cache = torch.zeros(
-      size=(1 , n_kv_heads , max_seq_len , head_dim),
-      dtype=self.dtype,
-      device=self.device
-    )
-
-    self.v_cache = torch.zeros(
-      size=(1 , n_kv_heads , max_seq_len , head_dim),
-      dtype=self.dtype,
-      device=self.device
-    )
+    #DON'T PREALLOCATE DURING INFERENCE
+    #we will allocate on first use
+    self.k_cache : None | torch.Tensor = None
+    self.v_cache : None | torch.Tensor = None 
 
     self.cache_len = 0
 
+  def _ensure_allocated(self , length : int):
+    #will only allocate once on first use
+
+    if self.k_cache is None:
+      alloc_len = min(length , self.max_seq_len)
+      self.k_cache = torch.zeros(
+        (1 , self.n_kv_heads , alloc_len , self.head_dim),
+        dtype = self.dtype,
+        device = self.device
+      )
+      self.v_cache = torch.zeros(
+        (1 , self.n_kv_heads , alloc_len , self.head_dim),
+        dtype = self.dtype,
+        device = self.device
+      )
+    
   def update(
       self,
       k : torch.Tensor,
-      v : torch.Tensor
+      v : torch.Tensor,
+      index : int |None = None
   ) -> Tuple[torch.Tensor , torch.Tensor]:
     """
     Update KV cache with given KV values
@@ -54,21 +63,51 @@ class KVCache:
     Returns:
         Tuple[torch.Tensor , torch.Tensor]: tuple of updated Key and Value
     """
+    self._ensure_allocated(1)
+    
     
     batch_size , n_heads , seq_len , head_dim = k.shape
+    if index is None:
+      end_pos = self.cache_len + seq_len
 
-    end_pos = self.cache_len + seq_len
+      self.k_cache[: , : , self.cache_len : end_pos] = k
+      self.v_cache[: , : , self.cache_len : end_pos] = v
 
-    self.k_cache[: , : , self.cache_len : end_pos] = k
-    self.v_cache[: , : , self.cache_len : end_pos] = v
+      self.cache_len = end_pos
 
-    self.cache_len = end_pos
-
-    return (
-      self.k_cache[: , : , self.cache_len].contiguous(),
-      self.v_cache[: , : , self.cache_len].contiguous()
-    )
+      return (
+        self.k_cache[: , : , self.cache_len].contiguous(),
+        self.v_cache[: , : , self.cache_len].contiguous()
+      )
     
+    else:
+      if index >= seq_len:
+        #expand the kv cache
+        new_size = min(index + 100 , self.max_seq_len)
+        new_k_cache = torch.zeros(
+          (1 , self.n_kv_heads , new_size , self.head_dim),
+          dtype = self.dtype,
+          device = self.device
+        )
+        new_v_cache = torch.zeros(
+          (1 , self.n_kv_heads , new_size , self.head_dim),
+          dtype = self.dtype,
+          device = self.device
+        )
+        new_k_cache[: , : , :seq_len , :] = self.k_cache
+        new_v_cache[: , : , :seq_len , :] = self.v_cache
+        self.k_cache = new_k_cache
+        self.v_cache = new_v_cache
+      self.k_cache[: , : , index , :] = k.squeeze(2)
+      self.v_cache[: , : , index , :] = v.squeeze(2)
+      
+      self.cache_len = max(self.cache_len , index + 1)
+      return (
+        self.k_cache[: , : , :self.cache_len , :].contiguous(),
+        self.v_cache[: , : , :self.cache_len , :].contiguous()
+      )
+
+      
 
   def reset(self):
 
