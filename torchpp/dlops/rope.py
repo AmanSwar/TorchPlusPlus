da1dc9fc8e7cde4c56ad290a2583ae7276d5be00
Aman <aman.swar001@gmail.com>
2025-12-12 12:39:07 +0530

    add feat[rope] : add qkrope kernel encapsulation

--- torchpp/dlops/rope.py
+++ torchpp/dlops/rope.py
@@ -1,7 +1,7 @@
 import torch
 
 import ropeApplyFunction
-
+import qkropeApplyFunction
 
 def rope_apply(
   x : torch.Tensor,
@@ -19,3 +19,23 @@ def rope_apply(
       torch.Tensor
   """
   return ropeApplyFunction.rope_apply_cuda(x , cos , sin)
+
+
+def qkrope_apply(
+  q : torch.Tensor,
+  k : torch.Tensor,
+  cos : torch.Tensor,
+  sin : torch.Tensor
+):
+  """
+  applies RoPE to Q and K
+  Args:
+      q (torch.Tensor) : query tensor
+      k (torch.Tensor) : key tensor
+      cos (torch.Tensor): cos cache
+      sin (torch.Tensor): sin cache
+
+  Returns:
+      Tuple[torch.Tensor, torch.Tensor]
+  """
+  return qkropeApplyFunction.qkrope_apply_cuda(q , k , cos , sin)
