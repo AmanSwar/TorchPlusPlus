8a2f978ef47d41a3c9e77a6a83de360df90e81d9
Aman <aman.swar001@gmail.com>
2025-12-14 13:28:30 +0530

    feat[dubug] : add benchmark function

---
+++ torchpp/debug/benchmark.py
@@ -0,0 +1,42 @@
+import torch
+
+import time
+
+def benchmark_model(
+  model : torch.nn.Module,
+  example_inputs,
+  warmup: int = 5,
+  steps : int = 10,
+  device : str | torch.device = torch.device("cuda")
+):
+  
+  model = model.to(device)
+  model.eval()
+
+
+  if isinstance(example_inputs, (list, tuple)):
+    inputs = tuple(inp.to(device) for inp in example_inputs)
+  else:
+    # single tensor
+    inputs = (example_inputs.to(device),)
+
+  # Warmup runs (no profiler) to reduce first-run overhead noise
+  with torch.no_grad():
+    for _ in range(warmup):
+        _ = model(*inputs)
+
+
+  start = time.monotonic()
+
+  for _ in range(steps):
+    out = model(*inputs)
+
+  final_time = time.monotonic() - start
+
+
+  print("Finish Benchmarking !")
+  print("\nResults:\n")
+  print(f"Time Taken : {(final_time / steps):.3f}")
+  print(f"Throughput  : {(1 / (final_time / steps)):.3f} samples/sec")
+
+
