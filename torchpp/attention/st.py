e1788cbb9abd79173752bc874c85031db82f964f
Aman <aman.swar001@gmail.com>
2026-01-29 05:59:39 +0530

    ST-Transformer init

---
+++ torchpp/attention/st.py
@@ -0,0 +1,17 @@
+#Spatio-Temporal Transformer implementation
+
+import torch
+import torch.nn as nn
+
+from flash_attn import flash_attn_func
+
+
+class SpaceTransformer(nn.Module):
+  pass
+
+class TemporalTransformer(nn.Module):
+  pass
+
+
+class ST_Transformer(nn.Module):
+  pass
