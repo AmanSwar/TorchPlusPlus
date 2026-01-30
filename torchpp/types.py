6ae2f85ff1fe95fc2e7a0415458fd6e15593aa72
Aman <aman.swar001@gmail.com>
2026-01-30 06:24:29 +0530

    feat : typing utils

---
+++ torchpp/types.py
@@ -0,0 +1,16 @@
+"""
+General dtypes
+
+"""
+
+import torch
+
+
+Int = int | None
+Float = float | None
+Bool = bool | None
+String = str | None
+List = list | None
+Tuple = tuple | None
+
+Tensor = torch.Tensor | None
